f0279a29c2804f8ff3735b0d19e594ae9ceac945
Sergey Kambalin <greyexpert@gmail.com>
2019-07-19 12:24:32 +0600

    Merge webpack configs

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,19 +1,17 @@
 const path = require('path');
+const merge = require('webpack-merge');
 
-module.exports = async ({ config, mode }) => {
-  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-  // You can change the configuration based on that.
-  // 'PRODUCTION' is used when building the static version of storybook.
-
-  config.module.rules.push({
-    test: /\.scss$/,
-    use: [
-      // "style-loader", // creates style nodes from JS strings
-      "css-loader", // translates CSS into CommonJS
-      "sass-loader" // compiles Sass to CSS, using Node Sass by default
+module.exports = async ({ config, mode }) => merge(config, {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'css-loader',
+          'sass-loader',
+        ],
+        include: path.resolve(__dirname, '../src/'),
+      },
     ],
-    include: path.resolve(__dirname, '../'),
-  });
-
-  return config;
-};
+  },
+});

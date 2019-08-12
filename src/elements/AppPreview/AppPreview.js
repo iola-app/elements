7d1857487c31ab422633a3bb86e0f8cafca3cf6f
Sergey Kambalin <greyexpert@gmail.com>
2019-08-12 20:24:35 +0600

    AppPreview component props

--- src/elements/AppPreview/AppPreview.js
+++ src/elements/AppPreview/AppPreview.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import element, { css } from '@iola/custom-element';
 
 import styles from './AppPreview.scss';
 
 @element({
   tag: 'iola-app-preview',
+  attrs: ['background', 'logo', 'primary-color'],
   styles: [
     css`
       :host {
@@ -15,10 +17,30 @@ import styles from './AppPreview.scss';
   ]
 })
 export default class AppPreview extends Component {
+  static propTypes = {
+    background: PropTypes.string,
+    logo: PropTypes.string,
+    primaryColor: PropTypes.string,
+  };
+
   render() {
+    const { background, primaryColor } = this.props;
+
+    const screenStyle = background && {
+      backgroundImage: `url(${background})`,
+    };
+
+    const buttonStyle = primaryColor && {
+      backgroundColor: primaryColor,
+    };
+
+    const logoStyle = {
+      // TODO: add support of logo attr
+    }
+
     return (
       <div className="app-preview">
-        <div className="screen">
+        <div className="screen" style={screenStyle}>
           <div className="status-bar">
             <div className="icons"></div>
             <div className="time">12:30</div>
@@ -31,7 +53,7 @@ export default class AppPreview extends Component {
             </span>
             <div className="forgot-password">Forgot password?</div>
           </div>
-          <div className="button">Sign In</div>
+          <div className="button" style={buttonStyle}>Sign In</div>
           <div className="button bordered">Sign Up</div>
           <div className="button bordered">Change Website URL</div>
         </div>
